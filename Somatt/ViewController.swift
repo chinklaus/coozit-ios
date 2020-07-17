20e8781d9b9e8f20a2a48f994eeb5456b2baadb0
chinklaus <chinklaus@gmail.com>
2020-07-17 11:36:38 +0800

    Initial Commit

---
+++ Somatt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Somatt
+//
+//  Created by 秦士翔 on 2020/7/17.
+//  Copyright © 2020 Somatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
